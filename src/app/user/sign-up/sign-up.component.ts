664811d7c6698cb81dafaed2b5c239d358bc4197
hm22-22 <celine1994@qq.com>
2021-01-24 23:11:37 +0100

    Add pseudo pwd check

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {SignUpInfo} from './signUpInfo';
-import { FormBuilder } from '@angular/forms';
+import { SignUpInfo } from './signUpInfo';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -8,23 +9,87 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  input : SignUpInfo ={
-    pseudo : "111",
-    password : "111",
-
+  input: SignUpInfo = {
+    pseudo: "",
+    password: "",
   }
-  passwordVerif : String = "6";
+  passwordSeconde: String = "";
 
-  constructor() {
-   }
+  rslUniquePseudo: String = '';
+  rslSecurePassword: String = '';
+  rslSamePassword: String = 'test';
+
+
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(customerData : any) {
-    console.warn('submit', customerData);
-    console.warn(this.passwordVerif);
+  onSubmit(customerData: any) {
+    if(this.readyToSubmit()){
+      this.userService.createUser(customerData);
+    }
+    else{
+      console.log("waiiiiiiiiiit");
+    }
 
+    
+  
+   
+  }
+
+  checkPseudo() :boolean{
+    if (this.input.pseudo != '') {
+      this.userService.checkUniquePseudo(this.input.pseudo).subscribe(
+        res => {
+          if (res) {
+            
+            this.rslUniquePseudo = "YES!";
+            return true;
+          } else {
+            this.rslUniquePseudo = "ToT";
+            return false;
+          }
+        }
+      )
+    }
+    else{
+      this.rslUniquePseudo='';
+    }
+
+    return false;
+  }
+
+  checkPasswordSecurity(): boolean {
+this.passwordSeconde="";
+this.rslSamePassword ="";
+
+return true;
+
+  }
+
+  checkSamePassword() :boolean{
+    if (this.input.password=="" || this.passwordSeconde==""){
+      this.rslSamePassword=""
+    }
+
+
+    if(this.userService.isSame(this.input.password,this.passwordSeconde)){
+      this.rslSamePassword =":)"
+      return true;
+    }
+    else{
+      this.rslSamePassword ="Noooo";
+    }
+    return false;
+
+  }
+
+
+  readyToSubmit() {
+
+    return this.checkSamePassword() && this.checkPasswordSecurity() && this.checkPseudo() && this.input.password!="" &&this.input.pseudo!="";
   }
 
 
