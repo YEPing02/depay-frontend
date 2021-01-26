f6c52c10a389ba2391dd884ca692ca2826caf427
hm22-22 <celine1994@qq.com>
2021-01-26 22:52:34 +0100

    add debounceTime for checking pseudo

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SignUpInfo } from './signUpInfo';
 import { UserService } from '../user.service';
+import { fromEvent, Observable, Subscription,Subject} from 'rxjs';
+import { pluck, debounceTime, throttleTime, distinct, distinctUntilChanged } from 'rxjs/operators';
+
+
+
 
 
 @Component({
@@ -8,7 +13,7 @@ import { UserService } from '../user.service';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
   input: SignUpInfo = {
     pseudo: "",
     password: "",
@@ -19,68 +24,107 @@ export class SignUpComponent implements OnInit {
   rslSecurePassword: String = '';
   rslSamePassword: String = 'test';
 
+  public keyUp = new Subject<KeyboardEvent>();
+  private subscription:Subscription;
 
   constructor(private userService: UserService) {
+    this.subscription = this.keyUp.pipe(debounceTime(1000),distinctUntilChanged()).subscribe(
+     x=> this.checkPseudo());
+    
+
   }
 
   ngOnInit(): void {
+    //this.testRebounceTime();
+  }
+
+  ngOnDestroy(): void {
+    
+    this.subscription.unsubscribe();
+  }
+
+  testRebounceTime() {
+    const id = document.getElementById("inputPseudo");
+    if (id != null) {
+      const clicks = fromEvent(id, 'keyup');
+      const result = clicks.pipe(debounceTime(1000));
+      result.subscribe(x => console.log(x, "rrrrrrr"));
+
+    }
+
+
   }
 
   onSubmit(customerData: any) {
-    if(this.readyToSubmit()){
+    if (this.readyToSubmit()) {
       this.userService.createUser(customerData);
     }
-    else{
+    else {
       console.log("waiiiiiiiiiit");
     }
 
-    
-  
-   
+
   }
 
-  checkPseudo() :boolean{
+  checkPseudo(): boolean {
     if (this.input.pseudo != '') {
-      this.userService.checkUniquePseudo(this.input.pseudo).subscribe(
-        res => {
-          if (res) {
-            
-            this.rslUniquePseudo = "YES!";
-            return true;
-          } else {
-            this.rslUniquePseudo = "ToT";
-            return false;
-          }
-        }
-      )
-    }
-    else{
-      this.rslUniquePseudo='';
-    }
+      const eleInputPseudo = document.getElementById("inputPseudo");
+      if (eleInputPseudo != null) {
+        //const keyup = fromEvent(eleInputPseudo, 'keyup');
+        //const result = keyup.pipe(debounceTime(1000),distinctUntilChanged());
+        
+        //result.subscribe(x =>{
+          const obUniquePseudo :Observable<boolean>= this.userService.checkUniquePseudo(this.input.pseudo);
 
+          obUniquePseudo.subscribe(res => {
+            if (res) {
+              this.rslUniquePseudo = "YES!";
+              return true;
+            }
+            else {
+              this.rslUniquePseudo = "ToT";
+              return false;
+            }
+          })
+        }
+        
+
+
+        //  )}
+
+
+        }
+         
+      
+      else {
+        this.rslUniquePseudo = '';
+      }
     return false;
+
   }
+
+
 
   checkPasswordSecurity(): boolean {
-this.passwordSeconde="";
-this.rslSamePassword ="";
+    this.passwordSeconde = "";
+    this.rslSamePassword = "";
 
-return true;
+    return true;
 
   }
 
-  checkSamePassword() :boolean{
-    if (this.input.password=="" || this.passwordSeconde==""){
-      this.rslSamePassword=""
+  checkSamePassword(): boolean {
+    if (this.input.password == "" || this.passwordSeconde == "") {
+      this.rslSamePassword = ""
     }
 
 
-    if(this.userService.isSame(this.input.password,this.passwordSeconde)){
-      this.rslSamePassword =":)"
+    if (this.userService.isSame(this.input.password, this.passwordSeconde)) {
+      this.rslSamePassword = ":)"
       return true;
     }
-    else{
-      this.rslSamePassword ="Noooo";
+    else {
+      this.rslSamePassword = "Noooo";
     }
     return false;
 
@@ -89,10 +133,18 @@ return true;
 
   readyToSubmit() {
 
-    return this.checkSamePassword() && this.checkPasswordSecurity() && this.checkPseudo() && this.input.password!="" &&this.input.pseudo!="";
+    return this.checkSamePassword() && this.checkPasswordSecurity() && this.userService.checkUniquePseudo(this.input.pseudo) && this.input.password != "" && this.input.pseudo != "";
   }
-
-
+  /*
+    searchInput = document.getElementById('inputPseudo');
+    const searchInput$ = fromEvent(this.searchInput,'keyup').pipe(
+      pluck('target','value'),
+      debounceTime(1000)
+    )
+    searchInput$.subscribe(
+        value => console.log(`我触发后每间隔一秒时间才会执行哦,当前输入的值为${value}`)
+    )
+  */
 
 }
 
