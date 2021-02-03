3929dbbc57531e6031b8a98c7a8076e6265a47c5
hm22-22 <celine1994@qq.com>
2021-02-03 01:02:33 +0100

    change app constant name

--- src/app/shared/AppSetting.ts
+++ src/app/shared/AppSetting.ts
@@ -1,4 +1,4 @@
-export class AppSetting{
-    public static ENDPOINT='http://localhost:8080';
-    public static DEFAULTIMAGE = "../../../assets/0_wZAcNrIWFFjuJA78.jpg";
+export class AppSetting {
+    public static ENDPOINT = 'http://localhost:8080';
+    public static DEFAULT_IMAGE = "../../../assets/0_wZAcNrIWFFjuJA78.jpg";
 }
